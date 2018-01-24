2e21af10d05fc4d4940309834f45d107326ed0ab
Francis Lavalliere <Francis.Lavalliere@acentera.com>
2018-01-24 12:57:19 -0500

    updated sidebar and blogs

---
+++ client/components/layout/Sidebarblogs.vue
@@ -0,0 +1,313 @@
+<template>
+  <aside class="menu app-sidebarblog animated fullheight" :class="{ slideInLeft: show, slideOutLeft: !show }" width="200px">
+    <p class="menu-label">
+    </p>
+    <div class="list">
+      <ul>
+        <li>
+          <span>Create new Post
+          <br/>
+          </span>
+        </li>
+        <li v-for="(item, index) in blogData">
+          <div v-if="item.draft" class="draft">draft</div>
+          <span @click="selectPost({ vue: this, item: item })">
+            <div class="postTitle">{{item.title}}</div>
+            <br/>
+            {{item.pubDate}}
+          </span>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<script>
+import Expanding from 'vue-bulma-expanding'
+import { mapGetters, mapActions } from 'vuex'
+import TreeView from 'components/TreeView'
+
+export default {
+  components: {
+    Expanding,
+    TreeView
+  },
+
+  props: {
+    show: Boolean,
+    jsonData: Array
+  },
+
+  data () {
+    return {
+      isReady: false
+    }
+  },
+  mounted () {
+    let route = this.$route
+    if (route.name) {
+      this.isReady = true
+      this.shouldExpandMatchItem(route)
+    }
+  },
+  computed: {
+    ...mapGetters({
+      menu: 'menuitems'
+    }),
+    blogData () {
+      return this.$store.state.app.sidebarblogData.json
+    }
+  },
+
+  methods: {
+    ...mapActions([
+      'expandMenu',
+      'selectPost'
+    ]),
+
+    isExpanded (item) {
+      return item.meta.expanded
+    },
+
+    toggle (index, item) {
+      this.expandMenu({
+        index: index,
+        expanded: !item.meta.expanded
+      })
+    },
+
+    incrementTotal: function () {
+      console.log('total aazza')
+    },
+
+    shouldExpandMatchItem (route) {
+      let matched = route.matched
+      let lastMatched = matched[matched.length - 1]
+      let parent = lastMatched.parent || lastMatched
+      const isParent = parent === lastMatched
+
+      if (isParent) {
+        const p = this.findParentFromMenu(route)
+        if (p) {
+          parent = p
+        }
+      }
+
+      if ('expanded' in parent.meta && !isParent) {
+        this.expandMenu({
+          item: parent,
+          expanded: true
+        })
+      }
+    },
+
+    generatePath (item, subItem) {
+      return `${item.component ? item.path + '/' : ''}${subItem.path}`
+    },
+
+    findParentFromMenu (route) {
+      const menu = this.menu
+      for (let i = 0, l = menu.length; i < l; i++) {
+        const item = menu[i]
+        const k = item.children && item.children.length
+        if (k) {
+          for (let j = 0; j < k; j++) {
+            if (item.children[j].name === route.name) {
+              return item
+            }
+          }
+        }
+      }
+    }
+  },
+
+  watch: {
+    $route (route) {
+      this.isReady = true
+      this.shouldExpandMatchItem(route)
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+@import '~bulma/sass/utilities/variables';
+@import '~bulma/sass/utilities/mixins';
+
+
+
+.app-sidebarblog {
+  position: relative;
+  top: 0px;
+  left: 0;
+  bottom: 0;
+  padding: 0px 0 0px;
+  width: auto;
+  min-width: 300px;
+  float: left;
+  max-height: 100%;
+  height: calc(100% - 50px);
+  z-index: 1024 - 1;
+  background: #FFF;
+  box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  .draft {
+    float: right;
+    color: red;
+  }
+
+  .postTitle {
+    width: 80%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis // This is where the magic happens
+  }
+
+  .tree-view-item-key {
+    padding-right:30px;
+  }
+
+  @include mobile() {
+    transform: translate3d(-180px, 0, 0);
+  }
+
+  .icon {
+    vertical-align: baseline;
+    &.is-angle {
+      position: absolute;
+      right: 10px;
+      transition: transform .377s ease;
+    }
+    &:not(.is-angle) {
+      position: relative;
+      right: 3px;
+      top: -1px;
+    }
+  }
+
+  .menu-label {
+    padding-left: 5px;
+  }
+
+  .menu-list {
+    li a {
+      &[aria-expanded="true"] {
+        .is-angle {
+          transform: rotate(180deg);
+        }
+      }
+    }
+
+    li a + ul {
+      margin: 0 10px 0 15px;
+    }
+  }
+
+  .list {
+
+    h2 {
+      font: 400 40px/1.5 Helvetica, Verdana, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+
+    li {
+      font: 200 20px/1.5 Helvetica, Verdana, sans-serif;
+      border-bottom: 1px solid #ccc;
+    }
+
+    li:last-child {
+      border: none;
+    }
+
+    li span {
+      text-decoration: none;
+      color: #000;
+      display: block;
+      width: 100%;
+      cursor: pointer;
+
+      -webkit-transition: font-size 0.3s ease, background-color 0.3s ease;
+      -moz-transition: font-size 0.3s ease, background-color 0.3s ease;
+      -o-transition: font-size 0.3s ease, background-color 0.3s ease;
+      -ms-transition: font-size 0.3s ease, background-color 0.3s ease;
+      transition: font-size 0.3s ease, background-color 0.3s ease;
+    }
+
+    li span:hover {
+      background: #f6f6f6;
+    }
+  }
+
+
+}
+
+.app-sidebartwo-fixed {
+  position: fixed;
+  top: 50px;
+  left: 0;
+  bottom: 0;
+  padding: 20px 0 50px;
+  width: auto;
+  min-width: 45px;
+  max-height: 100vh;
+  height: calc(100% - 50px);
+  z-index: 1024 - 1;
+  background: #FFF;
+  box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  .tree-view-item-key {
+    padding-right:30px;
+  }
+
+  @include mobile() {
+    transform: translate3d(-180px, 0, 0);
+  }
+
+  .icon {
+    vertical-align: baseline;
+    &.is-angle {
+      position: absolute;
+      right: 10px;
+      transition: transform .377s ease;
+    }
+    &:not(.is-angle) {
+      position: relative;
+      right: 3px;
+      top: -1px;
+    }
+  }
+
+  .menu-label {
+    padding-left: 5px;
+  }
+
+  .menu-list {
+    li a {
+      &[aria-expanded="true"] {
+        .is-angle {
+          transform: rotate(180deg);
+        }
+      }
+    }
+
+    li a + ul {
+      margin: 0 10px 0 15px;
+    }
+  }
+
+}
+
+
+</style>
---
+++ client/views/blogs/index.vue
@@ -0,0 +1,362 @@
+<template>
+  <div class="app-inner-content">
+    <div v-if="isRepoError()">
+      <div class="fullheight">
+
+          <article class="padleft tile is-child box">
+              <label class="label">Your current repository is in error. <br/><br/>Validate if you have a .git/config file, in your folder or ask for help on our gitter channel.</label>
+          </article>
+
+      </div>
+    </div>
+
+
+    <div class="fullheight" v-if="!isRepoError()">
+      <sidebarblogs :jsonData="sidebarblogData.json" :show="sidebartwo.opened && !sidebartwo.hidden"></sidebarblogs>
+      <div class="plekan-outerdiv tile is-ancestor is-vertical fullheight">
+
+          <article :class="{ hidden: selectedPost }" class="tile is-child box">
+              <label class="label">Select a post post from the left menu, or click on Create new post.</label>
+          </article>
+
+          <plekan></plekan>
+
+          <div v-if="selectedPost !== null" class="rightSide">
+
+          </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import Tooltip from 'vue-bulma-tooltip'
+import TreeView from 'components/TreeView'
+import { Sidebarblogs } from 'components/layout/'
+import { mapGetters } from 'vuex'
+import plekan from 'components/plekan/plekan.vue'
+// import Modal from './modals/InfoModalWidget'
+// import Vue from 'vue'
+
+// import Modal from './modals/InfoModal'
+
+const querystring = require('querystring')
+
+export default {
+  components: {
+    Tooltip,
+    TreeView,
+    // Modal,
+    Sidebarblogs,
+    plekan
+  },
+
+  data () {
+    return {
+      csrf: '',
+      showModal: false,
+      plaintext: '',
+      testTitle: '',
+      selectedObject: null,
+      type: 'Static',
+      selectedPage: null,
+      cipher: '',
+      userTransitKey: '',
+      extra: '?editMode=widget&apiPort=8081&jsPort=8091',
+      editing: false,
+      chart: [
+      ]
+    }
+  },
+
+  mounted: function () {
+    // $('.rightSide')[0].style.top = $('.app-sidebarblog').getBoundingClientRect().top
+    var self = this
+    console.log('is github')
+    console.log(this)
+    console.log(this.github)
+    this.$store.state.app.topbar.show = true
+    this.$bus.$on('staticHtmlSelected', function (data) {
+      console.log('reciev inc dotal modal')
+      console.log(this)
+      self.selectedObject = data
+      self.showModal = true
+    })
+
+    this.$bus.$on('staticHtmlEdit', function (data) {
+      console.log('reciev edidt of ...data')
+      console.log(data)
+      // self.selectedObject = data
+      // self.showModal = true
+      self.selectedPage = window.apiHost + '/widgetedit/' + data.original.Path + self.extra + '&widget=' + data.original.Path
+    })
+
+    this.refreshData()
+  },
+
+  computed: {
+    ...mapGetters({
+      session: 'session',
+      github: 'github',
+      pkginfo: 'pkg',
+      sidebarblogData: 'sidebarblogData',
+      sidebartwo: 'sidebartwo',
+      repoState: 'repoState',
+      selectedPost: 'selectedPost'
+    })
+  },
+
+  events: {
+    incrementTotal: function () {
+      console.log('total aaa')
+    }
+  },
+
+  methods: {
+    refreshData () {
+      var self = this
+      if (!this.$store.state.app.isLoaded) {
+        console.error('not loaded')
+        return setTimeout(function () {
+          self.refreshData()
+        }, 1000)
+      }
+
+      this.$httpApi.get(window.goHostUrl + '/blog/index.xml', {
+        responseType: 'document'
+      }).then((response) => {
+        console.error(response.data.childNodes[0].children[0])
+        var jsObj = JSON.parse(JSON.parse(JSON.stringify(window.xml2json(response.data.childNodes[0].children[0])).replace('undefined', '')))
+        console.error(jsObj.channel.item)
+        // self.$store.state.app.topbar.json = jsObj.channel.item
+
+        // this.$data.chart = response.data
+        // this.$store.commit('TOGGLE_SIDEBAR', false)
+        this.$store.commit('TOGGLE_SIDEBAR_BLOGDATA', true)
+        this.$store.commit('TOGGLE_BLOGDATA', jsObj.channel.item)
+      })
+      .catch((error) => {
+        console.error(error.stack)
+        this.$onError(error)
+      })
+    },
+    updateEditFrame () {
+    },
+
+    close () {
+      console.log('close here a')
+      this.showModal = false
+    },
+    /*
+    editObj () {
+      console.log('edit obj of a')
+    },
+    */
+    createWidget (obj) {
+      console.log('creating widget using')
+      console.log(obj)
+
+      var filetmp = obj.folder
+      var filepath = filetmp
+      var ctr = 0
+      while (filetmp[ctr++] === '/') {
+        filepath = filetmp.substring(1)
+      }
+
+      filetmp = obj.filename
+      var file = filetmp
+      ctr = 0
+      while (filetmp[ctr++] === '/') {
+        file = filetmp.substring(1)
+      }
+
+      var postData = {
+        filename: file,
+        path: filepath,
+        type: 'widget',
+        headers: obj.headers
+      }
+
+      var self = this
+      this.$httpApi.post(window.apiUrl + '/widgetupload', postData, {
+        // headers: {'TmpHeader': 'tmp'}
+      })
+      .then((response) => {
+        console.log('got response')
+        console.log(response)
+        self.refreshData()
+        self.close()
+        self.$notify({
+          title: 'Success',
+          message: 'Creation successful',
+          type: 'success'
+        })
+        this.selectedPage = window.goHostUrl + '/' + postData.path + '/' + postData.filename + self.extra
+      })
+      .catch((error) => {
+        console.log('got error eee')
+        console.log(error)
+        self.close()
+        self.$onError(error)
+      })
+    },
+
+    editObj (obj) {
+      console.log('edit obj of')
+      console.log(obj)
+      console.log(obj.original.Path)
+      this.selectedPage = window.goHostUrl + '/widgetedit/' + obj.original.Path + this.extra
+      this.showModal = false
+    },
+
+    closeModalBasic () {
+      console.log('close modal basic here')
+      this.selectedIndex = -1
+      this.showModal = false
+    },
+
+    AtoggleSidebar: function (f) {
+      this.$store.commit('TOGGLE_SIDEBAR_TWO', true)
+      this.$store.commit('TOGGLE_SIDEBAR_TWO_DATA', this.$data.chart)
+    },
+
+    incrementTotal: function () {
+      console.log('total aaa')
+    },
+
+    openWrench: function () {
+      console.log('azz')
+    },
+
+    encryptText: function () {
+      if (this.editing) {
+        return
+      }
+
+      this.$http.post('/api/transit/encrypt', querystring.stringify({
+        plaintext: this.plaintext,
+        key: this.userTransitKey
+      }), {
+        headers: {'X-CSRF-Token': this.csrf}
+      })
+
+      .then((response) => {
+        this.cipher = response.data.result
+        this.plaintext = ''
+        this.$notify({
+          title: 'Success',
+          message: 'Encryption successful',
+          type: 'success'
+        })
+      })
+
+      .catch((error) => {
+        this.$onError(error)
+      })
+    },
+    decryptText: function () {
+      if (this.editing) {
+        return
+      }
+
+      this.$http.post('/api/transit/decrypt', querystring.stringify({
+        cipher: this.cipher,
+        key: this.userTransitKey
+      }), {
+        headers: {'X-CSRF-Token': this.csrf}
+      })
+
+      .then((response) => {
+        this.plaintext = response.data.result
+        this.cipher = ''
+        this.$notify({
+          title: 'Success',
+          message: 'Decryption successful',
+          type: 'success'
+        })
+      })
+
+      .catch((error) => {
+        this.$onError(error)
+      })
+    },
+
+    clearPlaintext: function () {
+      this.plaintext = ''
+    },
+    clearCipher: function () {
+      this.cipher = ''
+    },
+    isRepoError () {
+      return (this.repoState.updating >= 5)
+    },
+
+    changeKey: function () {
+      this.editing = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .padleft {
+    // position: relative;
+    left:0px;
+    top:0px;
+    height:300px;
+    flex-grow: 0;
+    min-height:300px;
+    width:90%;
+  }
+
+  .app-inner-content {
+      height: 100%;
+      margin-top: 13px;
+  }
+  .button {
+    margin: 5px 0 0;
+  }
+
+  .fullheight {
+    height: 100%;
+    min-height:100%;
+  }
+
+  .control .button {
+    margin: inherit;
+  }
+
+  .fa-trash-o {
+    color: red;
+  }
+
+  .fa-info {
+    color: lightskyblue;
+  }
+
+
+  .rightSide.active {
+    right: 0px;
+  }
+
+  .rightSide {
+      position: fixed;
+      width: 200px;
+      height: 100%;
+      background-color: #f2f2f2;
+      right: -300px;
+      padding: 10px;
+      top: 105px;
+      border-left: 1px solid #ddd;
+      transition: all .3s;
+      z-index: 12;
+      box-shadow: 0px 3px 78px 0px rgba(0, 0, 0, 0.1);
+  }
+
+  .animated {
+      animation-duration: .377s;
+  }
+
+</style>
